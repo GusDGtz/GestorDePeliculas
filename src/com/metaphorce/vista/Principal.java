0d129708e9ccf952fc31084a7fd76d6791f15e59
Gus <gus.dom.gtz@gmail.com>
2025-05-23 19:34:55 -0600

    Nueva funcionalidad 2

--- src/com/metaphorce/vista/Principal.java
+++ src/com/metaphorce/vista/Principal.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Nuevo funcionalidad buscar por nombre");
+        System.out.println("Nueva funcionalidad");
         int opcion = 0;
         int contadorId = 6;
         System.out.println("Skill challenge 1");
